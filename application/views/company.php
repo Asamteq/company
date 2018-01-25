bf5544f5bd0fe589deda6c029d48c9eed0df87cb
wallirashid <wallirashid10@gmail.com>
2018-01-25 20:22:19 +0500

    new files added

---
+++ application/views/company.php
@@ -0,0 +1,122 @@
+<DOCTYPE HTML>
+<html>
+	<head>
+   	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>ASAM TEQ</title>
+        <link rel="stylesheet" href="<?php echo $this->config->item('base_url')?>assets/css/bootstrap.css" type="text/css">
+         <link rel="stylesheet" href="<?php echo $this->config->item('base_url')?>assets/css/style.css" type="text/css">
+         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700" rel="stylesheet">
+        <script src="<?php echo $this->config->item('base_url')?>assets/js/jquery.js" type="text/JavaScript"></script>
+        <script src="<?php echo $this->config->item('base_url')?>assets/js/bootstrap.js" type="text/JavaScript"></script>
+    </head>
+ <body>
+ <div id="navigation-section">
+ 	<div id="logo">
+    	
+    </div>
+ 	<div id="menu">
+		<ul>
+        	<li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="#services-section">Services</a></li>
+            <li><a href="">Our Work</a></li>
+            <li><a href="">Carrer</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="" class="btn btn-generic-style">Contact Us</a></li>
+        </ul>
+    </div>
+ </div><!--eend of navigation section-->
+ <div id="hero-image" class="parallex">
+ 	<div id="hero-text">
+     	<h3>Welcome to Web Design & Development</h3>
+       	<h1>Web Design & Development Services</h1>
+        <h2>Creating solutions With More Features To Reach The Market Faster</h2> 
+        <div id="info-box">
+        	<h2>Get Free Quets<br>Contact to our site</h2>
+        </div>
+     </div>
+</div><!--end of hero data-->
+<div id="about-data">
+    <h3>About</h3>
+    <div class="underline"></div>
+     <div class="container-fluid">
+        <div class="row">
+         <div class="col-md-6">
+             <div id="about-content">
+                <h4>About</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy <br>text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It<br> was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                 <a href=""></a>
+            </div>
+             </div>
+         <div class="col-md-6">
+             <img src="../../assets/images/about-picture.jpg" width="100%">
+            </div>
+         </div>    
+     </div>
+ </div><!--end of about data-->
+                    <!--creation of services section-->
+    <div id="services-section">
+        <h3>Services We Offered</h3>
+        <div class="underline-services"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3  col-md-offset services-coloumn-style">
+                    <h4>Web Designing</h4>
+                    <img src="../../assets/images/web-design.jpg" width="100%">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                <div class="col-md-4 col-md-offset-1 services-coloumn-style">
+                    <h4>Web Development</h4>
+                     <img src="../../assets/images/web-develop.jpg" width="100%">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                <div class="col-md-3 col-md-offset-1 services-coloumn-style">
+                    <h4>Seo</h4>
+                      <img src="../../assets/images/seo.jpg" width="100%">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                  <div class="col-md-3  col-md-offset services-coloumn-style">
+                    <h4>Mobile Development</h4>
+                      <img src="../../assets/images/mobile-develop.png" width="100%">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                 <div class="col-md-4 col-md-offset-1 services-coloumn-style">
+                    <h4>Ios Development</h4>
+                    <img src="../../assets/images/ios-develop.jpg" width="80%">
+                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                <div class="col-md-3 col-md-offset-1 services-coloumn-style">
+                    <h4>Game Development</h4>
+                    <img src="../../assets/images/game-develop.jpg" width="100%">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                </div>
+            </div>
+        </div><!--end of services section-->
+     <div id="footer-section">
+        <div class="container">
+            <div class="col-md-4 footer-information">
+                <h4>Contact Infromation</h4>
+                <label>Phone:</label><br>
+                <label>Email:</label><br>
+                <label>Skype:</label>
+            </div>
+             
+                <div class="col-md-8">
+                    <h4>Office:&nbsp;&nbsp;&nbsp;Pakistan Office 504 Liberty Tower</h4>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4189.5606058968815!2d74.31772515108666!3d31.469715586419827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391906a9efb63f51%3A0xfdeb76efa466feee!2sNew+Liberty+Tower!5e0!3m2!1sen!2s!4v1516892381035" width="600px" height="200px" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+         
+         
+         
+         
+         </div>
+     
+     </div>
+ 
+ 
+ 
+ 
+ 
+ </body>   
+</html>
